2de66a11f32d498a2c085a22e3f5cfe9f879a7e6
Addel <sigmamediacode@gmail.com>
2023-10-11 22:42:30 -0400

    ejercicio 1

--- Ejercicios de Fogueo/01_Ejercicio.js
+++ Ejercicios de Fogueo/01_Ejercicio.js
@@ -1,1 +1,13 @@
-console.log('Hello Word');
+// console.log('Hello Word');
+
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = 5 + 1;
+b = 10 + 5;
+c = 'I am a' + ' String!';
+console.log(a);
+console.log(c);
