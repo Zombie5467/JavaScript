ec346bddbc01beebd3a07bcbcb14254311a5d19b
Addel <sigmamediacode@gmail.com>
2025-07-28 14:33:16 -0400

    ejercicio semana 2 completado con router y middelware

--- Practicas Node/src/Semana 02/app.js
+++ Practicas Node/src/Semana 02/app.js
@@ -5,77 +5,25 @@
      * Ejercicio: Agrega rutas para manejar POST y DELETE.
  */
 
-import express from "express";
 import dotenv from "dotenv";
-import { USERS_BBDD } from "./bbdd.js"; // Importamos la base de datos de usuarios
+import express from "express";
+import accountRouter from "../Semana 02/routes/account.js"; // Importamos las rutas de cuenta
+
+//----------------------------------------------------------------------------------
 
 dotenv.config(); // Importamos dotenv para manejar variables de entorno
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const expressApp = express(); // Este express() es el mismo que importamos mas arriba.
+
+//----------------------------------------------------------------------------------
+
+//Usando endpoints
 expressApp.use(express.json()); // Middleware para parsear JSON
 expressApp.use(express.text()); // Middleware para parsear texto
+expressApp.use( "/account", accountRouter); // un path principal para usar router
 
-//----------------------------------------------------------------------------------
-
-// Obtener los detalles de una cuenta
-// Usamos get para obtener los datos del usuario a traves de su id (guid)
-expressApp.get('/account/:guid', (req, res) => {
-    const {guid} = req.params;
-    const user = USERS_BBDD.find(user => user.guid === guid);
-
-    if(!user) return res.status(404).send();
-
-    return res.send(user);
-});
-
-//----------------------------------------------------------------------------------
-
-//Crear una cuenta nueva a partir de guid y name
-expressApp.post('/account', (req, res) => {
-    const {guid, name } = req.body;
-
-    if(!guid || !name) return res.status(400).send();
-
-    const user = USERS_BBDD.find((user) => user.guid === guid);
-    if (user) return res.status(409).send();
-
-    USERS_BBDD.push({
-        guid,
-        name,
-    });
-
-    return res.send();
-});
-
-//----------------------------------------------------------------------------------
-
-//Actualizar el nombre de una cuenta
-expressApp.patch('/account/:guid', (req, res) => {
-    const {guid} = req.params;
-    const {name} = req.body;
-
-    if(!name) return res.status(400).send();
-    const user = USERS_BBDD.find((user) => user.guid === guid);
-
-    if(!user) res.status(404).send();
-
-    user.name = name; // Actualizamos el nombre del usuario
-    return res.send(user); // Devolvemos el usuario actualizado
-
-});
-
-//----------------------------------------------------------------------------------
-
-//Eliminar una cuenta
-expressApp.delete('/account/:guid', (req, res) => {
-    const {guid} = req.params;
-    const userIndex = USERS_BBDD.findIndex((user) => user.guid === guid);
-
-    if(userIndex === -1) return res.status(404).send();
-
-    USERS_BBDD.splice(userIndex, 1); // Elimina el usuario del array
-    return res.send();
-
+expressApp.get("/raiz", (req, res) => {
+  res.send();
 });
 
 //----------------------------------------------------------------------------------
---
+++ Practicas Node/src/Semana 02/routes/account.js
@@ -0,0 +1,70 @@
+import express from "express";
+import { USERS_BBDD } from "../bbdd.js"; // Importamos la base de datos de usuarios
+
+const accountRouter = express.Router();
+
+//Usando middleware
+accountRouter.use((req, res, next) => {
+    console.log(req.ip);
+    next();
+});
+
+// Obtener los detalles de una cuenta
+// Usamos get para obtener los datos del usuario a traves de su id (guid)
+accountRouter.get("/:guid", (req, res) => {
+  const { guid } = req.params;
+  const user = USERS_BBDD.find((user) => user.guid === guid);
+
+  if (!user) return res.status(404).send();
+
+  return res.send(user);
+});
+//----------------------------------------------------------------------------------
+
+//Crear una cuenta nueva a partir de guid y name
+accountRouter.post("", (req, res) => {
+  const { guid, name } = req.body;
+
+  if (!guid || !name) return res.status(400).send();
+
+  const user = USERS_BBDD.find((user) => user.guid === guid);
+  if (user) return res.status(409).send();
+
+  USERS_BBDD.push({
+    guid,
+    name,
+  });
+
+  return res.send();
+});
+
+//----------------------------------------------------------------------------------
+
+//Actualizar el nombre de una cuenta
+accountRouter.patch("/:guid", (req, res) => {
+  const { guid } = req.params;
+  const { name } = req.body;
+
+  if (!name) return res.status(400).send();
+  const user = USERS_BBDD.find((user) => user.guid === guid);
+
+  if (!user) res.status(404).send();
+
+  user.name = name; // Actualizamos el nombre del usuario
+  return res.send(user); // Devolvemos el usuario actualizado
+});
+
+//----------------------------------------------------------------------------------
+
+//Eliminar una cuenta
+accountRouter.delete("/:guid", (req, res) => {
+  const { guid } = req.params;
+  const userIndex = USERS_BBDD.findIndex((user) => user.guid === guid);
+
+  if (userIndex === -1) return res.status(404).send();
+
+  USERS_BBDD.splice(userIndex, 1); // Elimina el usuario del array
+  return res.send();
+});
+
+export default accountRouter;
